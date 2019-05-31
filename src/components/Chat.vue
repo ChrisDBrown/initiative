14bb852c326e333616452fa32d1ca7b3e3d5b823
Chris D Brown <owner@chrisdbrown.co.uk>
2019-05-31 22:11:52 +0100

    feat: Basic setup

---
+++ src/components/Chat.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card mt-3">
+    <div class="card-body">
+      <div class="card-title">
+        <h3>Chat Group</h3>
+        <hr>
+      </div>
+      <div class="card-body">
+        <div class="messages" />
+      </div>
+    </div>
+    <div class="card-footer">
+      <form @submit.prevent="sendMessage">
+        <div class="gorm-group">
+          <label for="user">User:</label>
+          <input
+            v-model="user"
+            type="text"
+            class="form-control"
+          >
+        </div>
+        <div class="gorm-group pb-3">
+          <label for="message">Message:</label>
+          <input
+            v-model="message"
+            type="text"
+            class="form-control"
+          >
+        </div>
+        <button
+          type="submit"
+          class="btn btn-success"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
